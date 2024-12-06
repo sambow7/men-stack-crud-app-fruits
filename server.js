e43382d78597dd5bbb7825b9fadfac180c5c4b5e
Sam Bowen <samhbowen@gmail.com>
2024-12-06 13:42:48 -0800

    template save

---
+++ server.js
@@ -0,0 +1,12 @@
+// Here is where we import modules
+// We begin by loading Express
+const express = require('express');
+
+const app = express();
+
+
+app.get()
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
+
